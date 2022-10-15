6f07a8597c4ca38dc99f8f21f4d68a8189869da5
Ibrahim Khan <113096720+ikon7869@users.noreply.github.com>
2022-10-15 19:21:24 +0530

    Update Nftmarketplace.sol

--- Nftmarketplace.sol
+++ Nftmarketplace.sol
@@ -1,77 +1,116 @@
 //SPDX-License-Identifier:MIT
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+error PriceIncorrect();
+
 pragma solidity ^0.8.16;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-error PriceInvalid();
-error NftnotInMarket();
-error NotOwner();
-
-contract Nftmarket is ERC721URIStorage, Ownable {
+contract Nftmarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private tokenId;
+    Counters.Counter private tokenIds;
+    Counters.Counter private itemsSold;
+    
+    address payable immutable owner;
+    uint256 constant listingPrice = 0.0009 ether;
 
-    struct Nftlistings {
-        uint256 price;
-        address seller;
-    }
-
-    constructor() ERC721("MYNFT","NFT"){}
     
 
-    mapping (uint256 => Nftlistings) _listings;
+    struct NftItems {
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        address payable owner;
+        bool sold;
+    }
+    event NftmarketItemCreated( 
+        uint256 indexed tokenId,
+        address seller, 
+        address buyer,  
+        uint256 price, 
+        bool sold);
 
-    event NftTransfer(uint256 tokenId, string tokenURI, uint256 price, address to);
-
-    function  createNft(string calldata tokenURI) public {
-        tokenId.increment();
-        uint256 currentId = tokenId.current();
-        _safeMint(msg.sender,currentId);
-        _setTokenURI(currentId,tokenURI);
-        emit NftTransfer(currentId,tokenURI,0,msg.sender);
+    constructor () ERC721("NFT Marketplace","MyNft") {
+        owner = payable(msg.sender);
     }
 
-    function listNft(uint256 _tokenId, uint256 price) public {
-        require(price > 0,"Price should be greater than zero!!");
-        approve(address(this),_tokenId);
-        transferFrom(msg.sender,address(this),_tokenId);
-        _listings[_tokenId] = Nftlistings(price,msg.sender);
-        emit NftTransfer(_tokenId,"",price,address(this));
+    mapping (uint256 => NftItems) nftItemsId;
+
+    function createNftToken(string memory tokenURI, uint256 price) public payable returns(uint256){
+        tokenIds.increment();
+        uint256 tokenId = tokenIds.current();
+        _mint(msg.sender,tokenId);
+        _setTokenURI(tokenId,tokenURI);
+        createNftItem(tokenId,price);
+        return tokenId;
     }
 
-    function buyNft(uint256 _tokenId) public payable {
-        Nftlistings memory listings = _listings[_tokenId];
-        if (listings.price > 0) {
-            revert NftnotInMarket();
+    function createNftItem(uint256 tokenId,uint256 price) private {
+        if (price <= 0) {
+            revert PriceIncorrect();
         }
-        require(msg.value == listings.price, "Price is incorrect!!");
-        transferFrom(address(this),msg.sender,_tokenId);
-        emit NftTransfer(_tokenId,"",0,msg.sender);
+        if (msg.value != listingPrice) {
+            revert PriceIncorrect();
+        }
+        nftItemsId[tokenId] = NftItems (
+            tokenId,
+            price,
+            payable(msg.sender),
+            payable(address(this)),
+            false
+        );
+        _transfer(msg.sender,address(this),tokenId);
+        emit NftmarketItemCreated(tokenId,msg.sender,address(this),price,false);
     }
 
-    function cancelListing(uint256 _tokenId) public {
-        Nftlistings memory listings = _listings[_tokenId];
-        require(listings.price > 0 ,"Nft is not in the market !!");
-        require(listings.seller == msg.sender, "You are not the owner!!");
-        transferFrom(address(this),msg.sender,_tokenId);
-        clearListing(_tokenId);
-        emit NftTransfer(_tokenId,"",0,msg.sender);
-
+    function buyNftItem(uint256 tokenId) public payable{
+        address seller = nftItemsId[tokenId].seller;
+        if (msg.value != nftItemsId[tokenId].price) {
+            revert PriceIncorrect();
+        }
+        nftItemsId[tokenId].owner = payable(msg.sender);
+        nftItemsId[tokenId].seller = payable(address(0));
+        nftItemsId[tokenId].sold = true;
+        itemsSold.increment();
+        _transfer(msg.sender,address(this),tokenId);
+        (bool success,) = payable(owner).call{value : listingPrice}("");
+        require(success);
+        (bool success1,) = payable(seller).call{value : msg.value}("");
+        require(success1);
     }
 
-    function withdraw() public onlyOwner {
-        uint256 balance  = address(this).balance;
-        require (balance > 0, "Balance is zero");
-        payable(owner()).transfer(balance);
-    }
-        
+    function fetchNfts() public view returns(NftItems[] memory){
+        uint256 itemCount = tokenIds.current();
+        uint256 unSoldNftCount = itemCount - itemsSold.current();
+        uint256 currentIndex = 0;
 
-    function clearListing(uint256 _tokenId) private {
-        _listings[_tokenId].price = 0;
-        _listings[_tokenId].seller = address(0);
+        NftItems[] memory items = new NftItems[](unSoldNftCount);
+        for (uint256 i = 0; i < itemCount; i++){
+            if (nftItemsId[i+1].owner == address(this)){
+                uint256 currentId = i + 1;
+                NftItems storage currentItem = nftItemsId[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
     }
 
+    function fetchMyNfts() public view returns(NftItems[] memory) {
+        uint256 itemCount = tokenIds.current();
+        uint256 unSoldNftCount = itemCount - itemsSold.current();
+        uint256 currentIndex = 0;
+
+        NftItems[] memory items = new NftItems[](unSoldNftCount);
+        for (uint256 i = 0; i < itemCount; i++ ){
+            if(nftItemsId[i+1].owner == msg.sender){
+                uint256 currentId = i + 1;
+                NftItems storage currentItem = nftItemsId[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
 }
