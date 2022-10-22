3a28111d93a9ba7cedf7486a3caa830bc525ebf1
Ibrahim Khan <113096720+ikon7869@users.noreply.github.com>
2022-10-22 13:09:22 +0530

    Update Ecommerce.sol

--- Ecommerce.sol
+++ Ecommerce.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.16;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "PriceConverter.sol";
+
 error PriceIncorrectError();
 error NotAuthorised();
 
 contract Ecommerce {
-    using PriceConverter for uint256;
     struct Products {
         string Name;
         string Description;
@@ -18,9 +17,20 @@ contract Ecommerce {
         bool isdelivered;
     }
 
-    AggregatorV3Interface private priceFeed;
+    AggregatorV3Interface internal eth_in_usd;
     Products[] product;
 
+    function ethInUsd() public view returns(uint) {
+          (,uint price,,,) = eth_in_usd.latestRoundData();
+        return (price * 1e18);
+   }
+    
+    function usdValue(uint _amount) public view returns(uint) {
+        uint Valueprice = ethInUsd();
+        uint AmountIndollors = (Valueprice * _amount) / 1e18;
+        return AmountIndollors;
+    }
+   
     function registerProduct(string memory _name, string memory _desc, uint256 _price) private {
         Products memory prod;
         if (_price < 0){
@@ -31,7 +41,7 @@ contract Ecommerce {
         }
         prod.Name = _name;
         prod.Description = _desc;
-        prod.price = _price * 50 * 1e18;
+        prod.price = usdValue(_price);
         prod.seller = msg.sender;
         uint256 counter;
         counter ++;
@@ -41,7 +51,8 @@ contract Ecommerce {
     }
 
     function buyProd(uint256 _prodId) payable public {
-        if (msg.value.getConversionRate(priceFeed) != product[_prodId-1].price) {
+        uint256 priceInUsd = usdValue(msg.value);
+        if (priceInUsd != product[_prodId-1].price) {
             revert PriceIncorrectError();
         }
 
